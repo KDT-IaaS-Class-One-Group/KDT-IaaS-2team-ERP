4a83f83c1def4320a9ea82d8dfd0009a945bcc76
leeyuan <dbdks32123@gmail.com>
2023-12-27 18:49:20 +0900

    layout : 로그인 폼 나오게

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,9 +4,7 @@ import React from "react";
 import { useState } from "react";
 
 import  MenuTab  from  "@/components/admin/MenuTab"
-// import { LoginForm } from  "@/components/admin/LoginForm"
-
-
+import  LoginForm  from  "@/components/admin/LoginForm"
 
 const Admin = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,7 +12,11 @@ const Admin = () => {
   return (
     <>
       <h1>관리자 페이지</h1>
-      <MenuTab />
+      {isLoggedIn ? (
+        <MenuTab />
+      ) : (
+        <LoginForm />
+      )}
     </>
   );
 };
