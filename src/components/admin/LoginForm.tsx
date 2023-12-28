d52d5b31c6cae7b614f612958f6a02d0cebb860a
leeyuan <dbdks32123@gmail.com>
2023-12-28 14:41:43 +0900

    feat : Login logic 구현중

--- src/components/admin/LoginForm.tsx
+++ src/components/admin/LoginForm.tsx
@@ -4,8 +4,10 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
 
   const router = useRouter();
 
@@ -16,17 +18,17 @@ const LoginForm = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username,
+        userId,
         password,
       }),
     });
-
+  
     if (response.status === 200) {
       const data = await response.json();
-
+  
       // 로그인 성공
-      setUsername(data.username);
-      router.push("/admin");
+      setIsLoggedIn(true);
+      setToken(data.token);
     } else {
       // 로그인 실패
       alert("로그인 실패!");
@@ -36,7 +38,7 @@ const LoginForm = () => {
   return (
     <div>
       <h2>로그인</h2>
-      <input type="text" placeholder="사용자 ID" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="text" placeholder="사용자 ID" value={userId} onChange={(e) => setUserId(e.target.value)} />
       <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button onClick={handleLogin}>로그인</button>
     </div>
