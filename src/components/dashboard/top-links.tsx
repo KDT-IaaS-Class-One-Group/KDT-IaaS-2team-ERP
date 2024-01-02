8e75dd88d66e340db53c8e305dfd8d9641a2a38f
leeyuan <dbdks32123@gmail.com>
2024-01-02 16:34:58 +0900

    compo : dashboard top menu

---
+++ src/components/dashboard/top-links.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+  { name: '회원관리', href: '/admin/dashboard/member' },
+  { name: '구독상품관리', href: '/admin/dashboard/subscribe' },
+  { name: '통계분석', href: '/admin/dashboard/analysis' },
+  { name: '고객지원', href: '/admin/dashboard/service'}
+];
+
+export default function TopNavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+          className={clsx(
+            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            {
+              'bg-sky-100 text-blue-600': pathname === link.href,
+              'text-white': true,
+            },
+          )}
+        >
+          <p className="linkname">{link.name}</p>
+        </Link>
+      ))}
+    </>
+  );
+}
