d3fd2266a80d485b48c24c52ae9d043165bee40a
leeyuan <dbdks32123@gmail.com>
2024-01-03 14:35:46 +0900

    chore : file 이름 수정

---
+++ src/components/dashboard/Sidenav-b.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import NavLinks from '@/components/dashboard/member/Member-nav-links-b'
+import styles from "@/styles/adminsidenav.module.scss"
+
+export default function SideNav() {
+  return (
+    <div className={styles.sidemenu}>
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+          
+        </div>
+      </Link>
+      <div className={styles.sidelink}>
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        
+      </div>
+    </div>
+  );
+}
