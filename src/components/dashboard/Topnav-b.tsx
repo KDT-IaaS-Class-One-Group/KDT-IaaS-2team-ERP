d6f4804a07958c68335294f23bd0a8a678e735ae
leeyuan <dbdks32123@gmail.com>
2024-01-03 14:36:01 +0900

    chore : file 이름 수정

---
+++ src/components/dashboard/Topnav-b.tsx
@@ -0,0 +1,13 @@
+import TopNavLinks from '@/components/dashboard/Top-links-b'
+import styles from "@/styles/admintopnav.module.scss"
+
+export default function TopNav() {
+  return (
+    <div className={styles.topmenu}>
+      <div className={styles.toplink}>
+        <TopNavLinks />
+      
+      </div>
+    </div>
+  );
+}
