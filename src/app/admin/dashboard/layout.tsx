0358d147af9bc1152078ee5c3fb9db70b9a049ee
leeyuan <dbdks32123@gmail.com>
2024-01-03 14:33:35 +0900

    Docs : 레이아웃 수정

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,14 +1,13 @@
-import SideNav from '@/components/dashboard/sidenav';
-import styles from "@/styles/mypage.module.scss";
-import Topbar from "@/components/dashboard/Topbar";
+import styles from "@/styles/dashboard.module.scss";
+import Topbar from "@/components/dashboard/Topbar-b";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.main}>
-      <div><Topbar /></div>
-      <div className={styles.side}>
-        <SideNav />
+      <div>
+        <Topbar />
       </div>
+      <div className={styles.side}></div>
       <div className={styles.content}>{children}</div>
     </div>
   );
