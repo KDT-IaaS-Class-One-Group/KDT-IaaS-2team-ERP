9ecdb5529fa79afdec00e967d5fc622e444ed3f3
leeyuan <dbdks32123@gmail.com>
2024-01-04 19:07:21 +0900

    feat : admin/dashboard 토큰 확인 로직

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,11 +1,28 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
 import styles from "@/styles/dashboard.module.scss";
 import Topbar from "@/components/dashboard/Topbar-b";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+
+  const router = useRouter();
+  const [token, setToken] = useState<string | null>(null); 
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    setToken(storedToken);
+    if (!storedToken) {
+      router.push('/admin');
+    }
+
+  }, [router]);
+
   return (
     <div className={styles.main}>
       <div>
-        <Topbar />
+        <Topbar/>
       </div>
       <div className={styles.side}></div>
       <div className={styles.content}>{children}</div>
