0594c5376980b01fa888fac1752be9ac2b30ffd8
ohseungmin <ohmin2886@gmail.com>
2024-01-04 12:59:55 +0900

    글쓰기 페이지는 간단히 쓸 수 있는 내용만 생성

--- src/app/testPage/writingPage/page.tsx
+++ src/app/testPage/writingPage/page.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+"use client";
+import React from "react";
+import styles from "@/styles/test.module.scss";
 import Link from "next/link";
 
 export default function Page() {
   return (
-  <div id='root'>
-  <label htmlFor="">제목</label>
-  <input type="text" />
-  <label htmlFor="">내용</label>
-  <textarea></textarea>
-  <Link href="/testPage">
-  <button>글 등록</button>
-  </Link>
-  </div>
-  )
+    <div className={styles.root}>
+      <div>
+        <label htmlFor="title">
+          제목
+        </label>
+        <input type="text" id="title" className={`${styles.width} ${styles.margin}`} />
+      </div>
+      <div>
+        <label htmlFor="contents">내용</label>
+        <textarea id="contents" className={`${styles.width} ${styles.height}`}></textarea>
+      </div>
+      <Link href="/testPage">
+        <button>Go back to Test Page</button>
+      </Link>
+    </div>
+  );
 }
