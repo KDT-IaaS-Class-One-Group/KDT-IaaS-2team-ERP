3facd3dddb5f8032791d0b2a25ff800ecf6be0b3
ohseungmin <ohmin2886@gmail.com>
2024-01-04 17:41:04 +0900

    글쓰기 기능 구현 및 POST 요청 기능 작동 체크 및 이전으로 돌아가는 버튼까지 구현

--- src/app/testPage/writingPage/page.tsx
+++ src/app/testPage/writingPage/page.tsx
@@ -1,24 +1,88 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/test.module.scss";
 import Link from "next/link";
 
 export default function Page() {
+  const [formData, setFormData] = useState({
+    boardKey: "",
+    User_index: "",
+    title: "",
+    content: "",
+    date: "",
+    password: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/testPage/writingPage/create-post', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log("Board created successfully!");
+        // Handle success (redirect, update UI, etc.)
+      } else {
+        console.error("Failed to create board");
+        // Handle error (show message, retry, etc.)
+      }
+    } catch (err) {
+      console.error("Error creating board:", err);
+      // Handle error (show message, retry, etc.)
+    }
+  };
   return (
-    <div className={styles.root}>
+    <form onSubmit={handleSubmit} className={styles.root}>
       <div>
-        <label htmlFor="title">
-          제목
-        </label>
-        <input type="text" id="title" className={`${styles.width} ${styles.margin}`} />
+        <label htmlFor="title">제목</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={formData.title}
+          onChange={handleChange}
+          className={`${styles.width} ${styles.margin}`}
+          placeholder="Title"
+        />
       </div>
       <div>
         <label htmlFor="contents">내용</label>
-        <textarea id="contents" className={`${styles.width} ${styles.height}`}></textarea>
+        <textarea
+          name="content"
+          id="contents"
+          value={formData.content}
+          onChange={handleChange}
+          className={`${styles.width} ${styles.height}`}
+          placeholder="Content"
+        ></textarea>
       </div>
+      <div>
+        <label htmlFor="key">비밀번호</label>
+        <input
+          type="password"
+          name="password"
+          id="key"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Create Board</button>
+      {/* 'testPage'로 이동하는 링크 */}
       <Link href="/testPage">
         <button>Go back to Test Page</button>
       </Link>
-    </div>
+    </form>
   );
 }
