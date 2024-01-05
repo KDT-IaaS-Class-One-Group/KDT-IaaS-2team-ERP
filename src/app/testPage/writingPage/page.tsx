2bbd2a95a0854481666ec29e8d688564f68e5b02
ohseungmin <ohmin2886@gmail.com>
2024-01-05 15:54:50 +0900

    e 매개변수의 타입 지정 타입은 이벤트 타입 객체 지정

--- src/app/testPage/writingPage/page.tsx
+++ src/app/testPage/writingPage/page.tsx
@@ -14,12 +14,12 @@ export default function Page() {
     image: "",
   });
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
