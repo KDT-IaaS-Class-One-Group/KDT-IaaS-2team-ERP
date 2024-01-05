02db31a76edbbcac46a24fc58cd2a4fd354170f9
ohseungmin <ohmin2886@gmail.com>
2024-01-05 15:58:17 +0900

    testPage에서 customer 페이지로 코드 이동

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,12 +1,65 @@
+"use client";
 import Topbar from "@/components/Topbar/Topbar";
 import styles from "@/styles/customer.module.scss"
+import { useState, useEffect } from "react";
+import React from "react";
+import Link from "next/link";
 
-export default async function Page() {
+interface item {
+  title: string;
+  password: string;
+  content: string;
+}
+
+export default function Page() {
+  const [data, setData] = useState({ title: "", content: "", password: "" });
+  const [showContent, setShowContent] = useState(false); // 추가: 컨텐츠를 보여줄지 여부 상태
+  const [titles, setTitles] = useState([]);
+
+  useEffect(() => {
+    fetch("/testPage/getData") // 적절한 API 경로를 사용하여 데이터를 가져옵니다.
+      .then((response) => response.json())
+      .then((titles) => {
+        setTitles(titles);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const handleTitleClick = (
+    title: string,
+    password: string,
+    content: string
+  ) => {
+    const enteredPassword = prompt("암호를 입력하세요:");
+
+    if (enteredPassword === password) {
+      setData({ ...data, title: title, content: content });
+      setShowContent(true);
+    } else {
+      alert("잘못된 암호입니다.");
+    }
+  };
+
   return (
     <div className={styles.root}>
       <Topbar />
       <div className={styles.main}>
-       고객 QNA
+      {titles.map((item: item, index: number) => (
+        <h1
+          key={index}
+          onClick={() =>
+            handleTitleClick(item.title, item.password, item.content)
+          }
+        >
+          {item.title}
+        </h1>
+      ))}
+      <Link href="/testPage/writingPage">
+        <button>Go to Writing Page</button>
+      </Link>
+      {showContent && <p>내용 : {data.content}</p>}
       </div>
     </div>
   );
