2aa4b3f840e1708d2461839150e6cbd3f5247306
leeyuan <dbdks32123@gmail.com>
2024-01-05 16:56:35 +0900

    refactoring

--- src/app/admin/dashboard/subscription/sub-product/page.tsx
+++ src/app/admin/dashboard/subscription/sub-product/page.tsx
@@ -2,13 +2,16 @@ import NavLinks from "@/components/dashboard/subscription/Subscription-nav-links
 import styles from "@/styles/adminsidenav.module.scss";
 import SubsProduct from "@/components/dashboard/subscription/Subs-product-b";
 
+
 export default async function Page() {
   return (
     <>
       <div className={styles.sidelink}>
         <NavLinks />
       </div>
+      <main>
       <SubsProduct />
+      </main>
     </>
   );
 }
