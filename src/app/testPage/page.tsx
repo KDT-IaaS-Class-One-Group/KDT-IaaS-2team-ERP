3ea17b2386f6816477b3540fb245b6086067523c
ohseungmin <ohmin2886@gmail.com>
2024-01-05 15:47:27 +0900

    interface 작성으로 매개변수의 타입 지정 및 등록되있는 데이터를 모두 출력

--- src/app/testPage/page.tsx
+++ src/app/testPage/page.tsx
@@ -3,37 +3,59 @@ import { useState, useEffect } from "react";
 import React from "react";
 import Link from "next/link";
 
+interface item {
+  title: string;
+  password: string;
+  content: string;
+}
+
 export default function App() {
   const [data, setData] = useState({ title: "", content: "", password: "" });
+  const [showContent, setShowContent] = useState(false); // 추가: 컨텐츠를 보여줄지 여부 상태
+  const [titles, setTitles] = useState([]);
 
   useEffect(() => {
     fetch("/testPage/getData") // 적절한 API 경로를 사용하여 데이터를 가져옵니다.
       .then((response) => response.json())
-      .then((data) => {
-        const postData = data[0]; // 데이터는 배열의 첫 번째 요소에 있을 것입니다.
-        setData(postData); // 가져온 데이터를 상태에 설정합니다.
+      .then((titles) => {
+        setTitles(titles);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
-  const handleTitleClick = () => {
+  const handleTitleClick = (
+    title: string,
+    password: string,
+    content: string
+  ) => {
     const enteredPassword = prompt("암호를 입력하세요:");
 
-    if (enteredPassword === data.password) {
-      alert(`Contents: ${data.content}`);
+    if (enteredPassword === password) {
+      setData({ ...data, title: title, content: content });
+      setShowContent(true);
     } else {
       alert("잘못된 암호입니다.");
     }
   };
-  
+
   return (
     <div id="root">
-      <h1 onClick={handleTitleClick}>{data.title}</h1>
+      {titles.map((item: item, index: number) => (
+        <h1
+          key={index}
+          onClick={() =>
+            handleTitleClick(item.title, item.password, item.content)
+          }
+        >
+          {item.title}
+        </h1>
+      ))}
       <Link href="/testPage/writingPage">
         <button>Go to Writing Page</button>
       </Link>
+      {showContent && <p>내용 : {data.content}</p>}
     </div>
   );
 }
