ff969386e538c7f3f816c17381ff7b3440f186e1
최은철 <wndqhrgkdlel@gmail.com>
2024-01-05 11:36:32 +0900

    input value -> cart DB의 product_name으로 저장

--- src/app/subscription/[Sub_Index]/page.tsx
+++ src/app/subscription/[Sub_Index]/page.tsx
@@ -1,6 +1,6 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 import Link from "next/link";
 interface SubscriptionClientSideProps {
   Subs_Index: number;
@@ -11,26 +11,55 @@ interface SubscriptionClientSideProps {
 
 export default function SubscriptionClientSide() {
   const [data, setData] = useState<SubscriptionClientSideProps[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState("");
   const Subs_Index = useParams();
   const subs_index = Subs_Index.Sub_Index;
-  console.log(subs_index)
+  console.log(subs_index);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`/api/subscription/${subs_index}`);
-        const dataFromServer= await response.json();
-        console.log(dataFromServer)
+        const dataFromServer = await response.json();
+        console.log(dataFromServer);
         setData(dataFromServer);
       } catch (error) {
-        console.error('데이터를 불러오는 도중 오류 발생:', error);
+        console.error("데이터를 불러오는 도중 오류 발생:", error);
       }
     };
 
-    
-      fetchData();
-
+    fetchData();
   }, []);
+
+  const handleOrderButtonClick = async () => {
+    try {
+      // 선택된 상품이 없으면 경고 표시
+      if (!selectedProduct) {
+        alert("상품을 선택하세요.");
+        return;
+      }
+
+      // 'api/order'로 POST 요청 보내기
+      const response = await fetch(`/api/order`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          product: selectedProduct,
+        }),
+      });
+
+      if (response.ok) {
+        console.log("주문 성공!");
+        window.location.href = "/";
+      } else {
+        console.error("주문 실패:", response.statusText);
+      }
+    } catch (error) {
+      console.error("주문 요청 중 오류 발생:", error);
+    }
+  };
 
   return (
     <div>
@@ -41,9 +70,15 @@ export default function SubscriptionClientSide() {
           <p>Week: {item.Week}</p>
         </div>
       ))}
+      <input
+        type="text"
+        placeholder="상품선택"
+        value={selectedProduct}
+        onChange={(e) => setSelectedProduct(e.target.value)}
+      ></input>
       <Link href={`/order`}>
-                 <button>주문하기</button>
-     </Link>
+        <button onClick={handleOrderButtonClick}>주문하기</button>
+      </Link>
     </div>
   );
 }
