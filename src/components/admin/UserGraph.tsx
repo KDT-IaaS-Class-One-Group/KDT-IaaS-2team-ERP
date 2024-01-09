23ecb96267e28b25780422d57873b08f5fbed67a
최은철 <wndqhrgkdlel@gmail.com>
2024-01-09 10:08:13 +0900

    월별 가입자수 그래프

--- src/components/admin/UserGraph.tsx
+++ src/components/admin/UserGraph.tsx
@@ -5,24 +5,24 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 import axios from 'axios';
 
 const UserGraph = () => {
-  const [cashRangeData, setCashRangeData] = useState([]);
+  const [monthlyUserData, setMonthlyUserData] = useState([]);
 
   useEffect(() => {
-    // 서버에서 데이터를 가져오는 API 호출
+    // 서버에서 월별 가입자 수 데이터를 가져오는 API 호출
     axios.get('/api/userGraph')
       .then(response => {
-        setCashRangeData(response.data);
+        setMonthlyUserData(response.data);
       })
       .catch(error => {
-        console.error('Error fetching cash range user data:', error);
+        console.error('Error fetching monthly user data:', error);
       });
   }, []);
 
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <BarChart data={cashRangeData} margin={{ top: 20, right: 30, left: 20, bottom: 10 }}>
+      <BarChart data={monthlyUserData} margin={{ top: 20, right: 30, left: 20, bottom: 10 }}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="cash" />
+        <XAxis dataKey="month" />
         <YAxis />
         <Tooltip />
         <Legend />
