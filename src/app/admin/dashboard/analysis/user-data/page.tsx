8c62583883a1ce1333942c56be2aedb33a371f29
최은철 <wndqhrgkdlel@gmail.com>
2024-01-09 10:58:57 +0900

    컴포넌트 위치 변경

--- src/app/admin/dashboard/analysis/user-data/page.tsx
+++ src/app/admin/dashboard/analysis/user-data/page.tsx
@@ -1,7 +1,7 @@
 import NavLinks from "@/components/dashboard/analysis/Data-nav-links-b";
 import styles from "@/styles/adminsidenav.module.scss";
 import UserData from "@/components/dashboard/analysis/User-data-b";
-import UserGraph from "@/components/admin/UserGraph";
+import UserGraph from "@/components/dashboard/analysis/UserGraph";
 
 export default async function Page() {
   return (
