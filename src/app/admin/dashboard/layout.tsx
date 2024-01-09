6171e62eea4932a7b9493c61cb775728c646eb87
leeyuan <dbdks32123@gmail.com>
2024-01-09 18:08:58 +0900

    chore

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,28 +1,26 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import styles from "@/styles/dashboard.module.scss";
 import Topbar from "@/components/dashboard/Topbar-b";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-
   const router = useRouter();
-  const [token, setToken] = useState<string | null>(null); 
+  const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('token');
+    const storedToken = localStorage.getItem("token");
     setToken(storedToken);
     if (!storedToken) {
-      router.push('/admin');
+      router.push("/admin");
     }
-
   }, [router]);
 
   return (
     <div className={styles.main}>
       <div>
-        <Topbar/>
+        <Topbar />
       </div>
       <div className={styles.side}></div>
       <div className={styles.content}>{children}</div>
