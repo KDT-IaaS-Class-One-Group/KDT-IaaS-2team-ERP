3f4c3158e3d3988d897ebaaca83ce4aeff14c38b
leeyuan <dbdks32123@gmail.com>
2024-01-10 17:19:06 +0900

    chore

--- src/app/admin/dashboard/subscription/sub-product/page.tsx
+++ src/app/admin/dashboard/subscription/sub-product/page.tsx
@@ -1,17 +1,9 @@
-import NavLinks from "@/components/dashboard/subscription/Subscription-nav-links-b";
-import styles from "@/styles/adminuser.module.scss";
 import SubsProduct from "@/components/dashboard/subscription/Subs-product-b";
-
 
 export default async function Page() {
   return (
     <>
-      <div className={styles.sidelink}>
-        <NavLinks />
-      </div>
-      <main>
       <SubsProduct />
-      </main>
     </>
   );
 }
