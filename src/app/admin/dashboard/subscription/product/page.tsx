8cf1b5054fa6dc88ae99be110fa6686b20c4a747
leeyuan <dbdks32123@gmail.com>
2024-01-10 21:30:12 +0900

    feat : admin 상품관리 페이지

--- src/app/admin/dashboard/subscription/product/page.tsx
+++ src/app/admin/dashboard/subscription/product/page.tsx
@@ -1,13 +1,8 @@
-import NavLinks from "@/components/dashboard/subscription/Subscription-nav-links-b";
-import styles from "@/styles/adminsubscription.module.scss";
 import Product from "@/components/dashboard/subscription/Product-b";
 
 export default async function Page() {
   return (
     <>
-      <div className={styles.sidelink}>
-        <NavLinks />
-      </div>
       <Product />
     </>
   );
