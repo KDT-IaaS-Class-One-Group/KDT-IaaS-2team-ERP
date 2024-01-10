2a325b6e0e58bbd445ffa8bc93c538d354c71ca2
leeyuan <dbdks32123@gmail.com>
2024-01-10 17:20:31 +0900

    compo : admin subscription 구독관리

--- src/components/dashboard/subscription/Subs-product-b.tsx
+++ src/components/dashboard/subscription/Subs-product-b.tsx
@@ -1,21 +1,24 @@
 "use client";
-import React, { useState, ChangeEvent, useEffect } from "react";
-import styles from "@/styles/adminuser.module.scss";
+import React, { useState, useEffect, useCallback, ChangeEvent } from "react";
+import styles from "@/styles/order-b.module.scss";
+import NavLinks from "@/components/dashboard/subscription/Subscription-nav-links-b";
 
 interface SubscriptionInfo {
-  Subs_Index: string;
-  product_Index: string;
+  subs_index: string;
   name: string;
   week: string;
+  size: string;
   price: string;
 }
+
+const pageSize = 13; // 페이지당 표시할 항목 수
 
 export default function SubsProduct() {
   const [showForm, setShowForm] = useState(false);
   const [subs, setSubs] = useState<SubscriptionInfo[]>([]);
   const [pageInfo, setPageInfo] = useState({
     currentPage: 1,
-    pageSize: 10,
+    pageSize: 13,
     totalPages: 1,
   });
 
@@ -27,10 +30,10 @@ export default function SubsProduct() {
   };
 
   useEffect(() => {
-    fetchData(pageInfo.currentPage, 10);
+    fetchData(pageInfo.currentPage);
   }, [pageInfo.currentPage]);
 
-  const fetchData = async (page: number, pageSize: number) => {
+  const fetchData = async (page: number) => {
     try {
       const response = await fetch(
         `/api/subs-product?page=${page}&pageSize=${pageSize}`
@@ -49,10 +52,10 @@ export default function SubsProduct() {
   };
 
   const [subscriptionInfo, setSubscriptionInfo] = useState<SubscriptionInfo>({
-    Subs_Index: "",
-    product_Index: "",
+    subs_index: "",
     name: "",
     week: "",
+    size: "",
     price: "",
   });
 
@@ -77,7 +80,7 @@ export default function SubsProduct() {
       if (response.ok) {
         // 성공적인 응답 처리
         console.log("Subscription added successfully");
-        fetchData(pageInfo.currentPage, 10);
+        fetchData(pageInfo.currentPage);
       } else {
         // 오류 응답 처리
         console.error(`Error adding subscription: ${response.status}`);
@@ -85,10 +88,10 @@ export default function SubsProduct() {
 
       // 입력 폼 초기화
       setSubscriptionInfo({
-        Subs_Index: "",
-        product_Index: "",
+        subs_index: "",
         name: "",
         week: "",
+        size: "",
         price: "",
       });
       setShowForm(false); // 폼 닫기
@@ -98,91 +101,131 @@ export default function SubsProduct() {
     }
   };
 
+  const handleDelete = async (subs_index: string) => {
+    try {
+      const response = await fetch(`/api/subs-product/${subs_index}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        console.log("Subscription deleted successfully");
+        fetchData(pageInfo.currentPage);
+        alert("삭제 완료");
+      } else {
+        console.error(`Error deleting subscription: ${response.status}`);
+        alert("삭제 실패");
+      }
+    } catch (error) {
+      // 네트워크 오류 및 기타 예외 처리
+      console.error("Error deleting subscription:", error);
+    }
+  };
+
   return (
-    <div className={styles.subproduct}>
-      <h1>구독 상품 관리</h1>
-      <button onClick={() => setShowForm(!showForm)}>구독 상품 추가</button>
-      {showForm && (
-        <div>
-          <label>
-            Product Index:
-            <input
-              type="text"
-              name="product_Index"
-              value={subscriptionInfo.product_Index}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={subscriptionInfo.name}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Week:
-            <input
-              type="text"
-              name="week"
-              value={subscriptionInfo.week}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Price:
-            <input
-              type="text"
-              name="price"
-              value={subscriptionInfo.price}
-              onChange={handleChange}
-            />
-          </label>
-          <button onClick={handleSubmit}>추가</button>
-        </div>
-      )}
-      <div className="subscription">
-        <table className={styles.subscriptionTable}>
-          <thead>
-            <tr>
-              <th>Subs_Index</th>
-              <th>product_Index</th>
-              <th>name</th>
-              <th>week</th>
-              <th>price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {subs.map((sub) => (
-              <tr key={sub.Subs_Index}>
-                <td>{sub.Subs_Index}</td>
-                <td>{sub.product_Index}</td>
-                <td>{sub.name}</td>
-                <td>{sub.week}</td>
-                <td>{sub.price}</td>
+    <>
+      <div className={styles.sidelink}>
+        <NavLinks />
+      </div>
+      <div className={styles.main}>
+        <h1 className={styles.title}>구독 관리</h1>
+        <button
+          onClick={() => setShowForm(!showForm)}
+          className={styles.addButton}
+        >
+          추가
+        </button>
+        {showForm && (
+          <div className={styles.addSubscription}>
+            <label className={styles.addLabel}>
+              Name:  
+              <input
+                type="text"
+                name="name"
+                value={subscriptionInfo.name}
+                onChange={handleChange}
+                className={styles.addInput}
+              />
+            </label>
+            <label className={styles.addLabel}>
+              Week:  
+              <input
+                type="text"
+                name="week"
+                value={subscriptionInfo.week}
+                onChange={handleChange}
+                className={styles.addInput}
+              />
+            </label>
+            <label className={styles.addLabel}>
+              Size:  
+              <input
+                type="text"
+                name="size"
+                value={subscriptionInfo.size}
+                onChange={handleChange}
+                className={styles.addInput}
+              />
+            </label>
+            <label className={styles.addLabel}>
+              Price:  
+              <input
+                type="text"
+                name="price"
+                value={subscriptionInfo.price}
+                onChange={handleChange}
+                className={styles.addInput}
+              />
+            </label>
+            <button onClick={handleSubmit} className={styles.delButton}>추가</button>
+          </div>
+        )}
+        <div className={styles.orderContent}>
+          <table className={styles.orderTable}>
+            <thead>
+              <tr>
+                <th>subs_index</th>
+                <th>name</th>
+                <th>week</th>
+                <th>size</th>
+                <th>price</th>
+              <th></th>
               </tr>
+            </thead>
+            <tbody>
+              {subs.map((sub) => (
+                <tr key={sub.subs_index}>
+                  <td>{sub.subs_index}</td>
+                  <td>{sub.name}</td>
+                  <td>{sub.week}</td>
+                  <td>{sub.size}</td>
+                  <td>{sub.price}</td>
+                  <td>
+                  <button className={styles.delButton} onClick={() => handleDelete(sub.subs_index)}>삭제</button>
+                </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className={styles.pagination}>
+            {Array.from(
+              { length: pageInfo.totalPages },
+              (_, index) => index + 1
+            ).map((pageNumber) => (
+              <button
+                key={pageNumber}
+                className={`${styles.paginationButton} ${
+                  pageNumber === pageInfo.currentPage ? styles.active : ""
+                }`}
+                onClick={() => handlePageChange(pageNumber)}
+              >
+                {pageNumber}
+              </button>
             ))}
-          </tbody>
-        </table>
-        <div className={styles.pagination}>
-          {Array.from(
-            { length: pageInfo.totalPages },
-            (_, index) => index + 1
-          ).map((pageNumber) => (
-            <button
-              key={pageNumber}
-              className={`pagination-button ${
-                pageNumber === pageInfo.currentPage ? "active" : ""
-              }`}
-              onClick={() => handlePageChange(pageNumber)}
-            >
-              {pageNumber}
-            </button>
-          ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
