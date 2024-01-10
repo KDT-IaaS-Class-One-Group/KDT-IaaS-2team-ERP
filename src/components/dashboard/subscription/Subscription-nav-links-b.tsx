2fda5a590ee0fc89e588a37b6298ed0fb4dbcb4b
leeyuan <dbdks32123@gmail.com>
2024-01-10 12:29:12 +0900

    style : admin class 수정

--- src/components/dashboard/subscription/Subscription-nav-links-b.tsx
+++ src/components/dashboard/subscription/Subscription-nav-links-b.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "@/styles/adminnavlink.module.scss";
 
 const links = [
-  { name: '구독 서비스 관리', href: '/admin/dashboard/subscription/service' },
-  { name: '구독 상품 관리', href: '/admin/dashboard/subscription/sub-product' },
-  { name: '상품 관리', href: '/admin/dashboard/subscription/product' },
+  { name: "구독 서비스 관리", href: "/admin/dashboard/subscription/service" },
+  { name: "구독 상품 관리", href: "/admin/dashboard/subscription/sub-product" },
+  { name: "상품 관리", href: "/admin/dashboard/subscription/product" },
 ];
 
 export default function NavLinks() {
@@ -16,11 +16,7 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => (
-        <Link
-          key={link.name}
-          href={link.href}
-          className={styles.linkname}
-        >
+        <Link key={link.name} href={link.href} className={styles.linkname}>
           <p>{link.name}</p>
         </Link>
       ))}
