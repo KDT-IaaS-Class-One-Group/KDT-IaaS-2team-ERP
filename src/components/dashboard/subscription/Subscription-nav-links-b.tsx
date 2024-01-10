d552b705086e898aa320a0c66433b799ba64fe88
leeyuan <dbdks32123@gmail.com>
2024-01-10 11:43:25 +0900

    chore : admin sub nav link class 수정

--- src/components/dashboard/subscription/Subscription-nav-links-b.tsx
+++ src/components/dashboard/subscription/Subscription-nav-links-b.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
 import styles from "@/styles/adminnavlink.module.scss";
 
 const links = [
@@ -20,15 +19,9 @@ export default function NavLinks() {
         <Link
           key={link.name}
           href={link.href}
-          className={clsx(
-            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-            {
-              'bg-sky-100 text-blue-600': pathname === link.href,
-              'text-white': true,
-            },
-          )}
+          className={styles.linkname}
         >
-          <p className={styles.linkname}>{link.name}</p>
+          <p>{link.name}</p>
         </Link>
       ))}
     </>
