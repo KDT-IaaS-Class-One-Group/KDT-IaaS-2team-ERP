93c65c58f779b35f4a3c60779b8b23c89830aee8
leeyuan <dbdks32123@gmail.com>
2024-01-10 11:43:47 +0900

    chore : admins side nav class 수정

--- src/components/dashboard/Sidenav-b.tsx
+++ src/components/dashboard/Sidenav-b.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import NavLinks from '@/components/dashboard/user/User-nav-links-b'
-import styles from "@/styles/adminsidenav.module.scss"
+import Link from "next/link";
+import NavLinks from "@/components/dashboard/user/User-nav-links-b";
+import styles from "@/styles/adminsidenav.module.scss";
 
 export default function SideNav() {
   return (
@@ -9,14 +9,11 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
-          
-        </div>
+        <div className="w-32 text-white md:w-40"></div>
       </Link>
       <div className={styles.sidelink}>
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        
+        <div></div>
       </div>
     </div>
   );
