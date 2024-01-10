c752a9b41b601a53a772d102dd11c79d6dd61349
ohseungmin <ohmin2886@gmail.com>
2024-01-10 17:20:11 +0900

    모달 창은 단순히 보여주는 창에 불과하기 때문에 fetch를 써도 reply 불러오기불가능

--- src/components/customer/Modal.tsx
+++ src/components/customer/Modal.tsx
@@ -1,33 +1,16 @@
 // Modal.tsx
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "@/styles/customer.module.scss";
 
 interface ModalProps {
   title: string;
   content: string;
+  reply: string;
   closeModal: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ title, content, closeModal }) => {
-  const [reply, setReply] = useState<string>("");
-
-  useEffect(() => {
-    fetchReplyData();
-  }, []); // 페이지 로딩 시에만 실행
-
-  const fetchReplyData = async () => {
-    try {
-      const response = await fetch("/customer/getData"); // 서버의 엔드포인트 경로 확인 필요
-      const data = await response.json();
-      if (data && data.length > 0) {
-        // 서버로부터 받아온 데이터 중 reply 부분 가져오기
-        setReply(data[0].reply);
-      }
-    } catch (error) {
-      console.error("Error fetching reply data:", error);
-    }
-  };
+const Modal: React.FC<ModalProps> = ({ title, content, reply, closeModal }) => {
 
   return (
     <div className={styles.modalBackdrop} onClick={closeModal}>
@@ -37,7 +20,7 @@ const Modal: React.FC<ModalProps> = ({ title, content, closeModal }) => {
         </span>
         <h2>제목 : {title}</h2>
         <p>내용 : {content}</p>
-        <p>답변: {reply}</p>
+        <p>Reply: {reply}</p>
       </div>
     </div>
   );
