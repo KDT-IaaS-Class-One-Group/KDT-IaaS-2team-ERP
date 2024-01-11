9955850b1f5009c5b822a30e815d3d6264efe566
leeyuan <dbdks32123@gmail.com>
2024-01-11 14:47:19 +0900

    docs : admin Q&A 페이지

--- src/app/admin/dashboard/service/page.tsx
+++ src/app/admin/dashboard/service/page.tsx
@@ -1,13 +1,8 @@
-import NavLinks from "@/components/dashboard/service/Service-nav-links-b"
-import styles from "@/styles/adminuser.module.scss";
 import QA from "@/components/dashboard/service/QA-b";
 
 export default async function Page() {
   return (
     <>
-      <div className={styles.sidelink}>
-        <NavLinks />
-      </div>
       <QA />
     </>
   );
