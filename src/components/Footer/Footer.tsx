6bc3663b6fcca838b16a6f287f69477fe5b4f1ae
leeyuan <dbdks32123@gmail.com>
2024-01-18 21:12:01 +0900

    style: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,12 +10,11 @@ export default function Footbar() {
       <div className={styles.footdiv2}>
         <div className={styles.suvdiv1}>NTS</div>
         <div className={styles.suvdiv2}>
-          <p>이용약관 l 개인정보처리방침 l 카약 공식 블로그 l 전국 검사소 목록</p>
-          <p>대표이사 : 변무영</p>
-          <p>사업자 등록번호 : 338-88-00960</p>
-          <p>고객센터 : 070-7714-7734</p>
-          <p>팩스 : +82-02-6190-4211</p>
-          <p>본사 : 대전광역시 유성구 대학로 179번길 7-12, 디3동 204호</p>
+          <p>이용약관  l  개인정보처리방침</p>
+          <p>CEO : 그린</p>
+          <p>H.P : 070-3232-3232</p>
+          <p>FAX : +82-02-3232-3233</p>
+          <p>ADDRESS : 대전광역시 서구 대덕대로</p>
           <p>ⓒ 2023 MK. All rights reserved</p>
         </div>
       </div>
@@ -24,7 +23,8 @@ export default function Footbar() {
           <p>Copyright © Dobra palarnia kawy. Powered by Shopify, made in korea</p>
         </div>
         <div className={styles.suvdiv4}>
-          <p>인스타 , 페북, 유튜브 넣기</p>
+        <img className={styles.logo} src="https://img.freepik.com/free-vector/instagram-logo_1199-122.jpg?1&w=1060&t=st=1705577550~exp=1705578150~hmac=2b07ef4fd1df9dea1c1eba7fbfe6158271c8c26b6a21e95ffb1ca08eb95131a4" alt="instagram"></img>
+        <img className={styles.logo} src="https://img.freepik.com/free-vector/instagram-logo_1199-122.jpg?1&w=1060&t=st=1705577550~exp=1705578150~hmac=2b07ef4fd1df9dea1c1eba7fbfe6158271c8c26b6a21e95ffb1ca08eb95131a4" alt="instagram"></img>
         </div>
       </div>
     </div>
