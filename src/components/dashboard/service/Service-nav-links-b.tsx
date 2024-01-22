938c8023eb3a3d0b22b2571923b3f597a42b9a01
leeyuan <dbdks32123@gmail.com>
2024-01-22 18:01:57 +0900

    chore : admin qna

--- src/components/dashboard/service/Service-nav-links-b.tsx
+++ src/components/dashboard/service/Service-nav-links-b.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import styles from "@/styles/adminnavlink.module.scss";
 
 const links = [{ name: "Q&A", href: "/admin/dashboard/service" }];
 
 export default function NavLinks() {
+  const pathname = usePathname();
 
   return (
     <>
       {links.map((link) => (
-        <Link key={link.name} href={link.href} className={styles.linkname}>
+        <Link key={link.name} href={link.href} className={`${styles.linkbox} ${pathname === link.href ? styles.activeLink : ''}`}>
           <p>{link.name}</p>
         </Link>
       ))}
