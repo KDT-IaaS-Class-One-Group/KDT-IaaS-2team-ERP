b3fac88e94070c88e0c978e85e123fc2104bf80d
ohseungmin <ohmin2886@gmail.com>
2024-01-23 15:48:15 +0900

    비밀번호 데이터 출력도 마찬가지로 출력 스타일 수정

--- src/components/FindLogin/UserPasswordModal.tsx
+++ src/components/FindLogin/UserPasswordModal.tsx
@@ -5,6 +5,8 @@ import styles from "@/styles/customer.module.scss";
 const UserPasswordModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const [userId, setUserId] = useState("");
   const [email, setEmail] = useState("");
+  const [output, setOutput] = useState<string>('');
+  const [isOutputVisible, setIsOutputVisible] = useState<boolean>(false);
 
   const handleFindPassword = async () => {
     // 클라이언트에서 서버로 비밀번호 찾기 요청 보내기
@@ -22,9 +24,10 @@ const UserPasswordModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
       if (response.ok) {
         // 서버 응답이 성공이면
         if (data.password) {
-          window.alert(`Password: ${data.password}`);
+          setOutput(`Password: ${data.password}`);
+          setIsOutputVisible(true);
         } else {
-          window.alert("User not found");
+          window.alert("회원정보가 존재하지 않습니다.");
         }
       } else {
         // 서버 응답이 실패이면
@@ -43,6 +46,7 @@ const UserPasswordModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
         </span>
         {/* 비밀번호 찾기 모달 내용 */}
         <div className={styles.outline}>
+        <div className={isOutputVisible ? styles.hidden : ''}>
         <div className={`${styles.margin} ${styles.textLeft} ${styles.font}`}>회원 비밀번호 찾기</div>
         <div className={styles.flexSet}>
         <div className={styles.marginRight}>
@@ -52,6 +56,12 @@ const UserPasswordModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
         </div>
         <button className={styles.buttonSize} onClick={handleFindPassword}>Find Password</button>
         </div>
+        </div>
+                  {/* 출력을 보여줄지 여부에 따라 동적으로 스타일 적용 */}
+                  <h3 className={`${styles.centeredContainer} ${isOutputVisible ? '' : styles.hidden}`}>
+            비밀번호 찾기가 완료되었습니다.
+          <h2>{output}</h2>
+          </h3>
         </div>
       </div>
     </div>
