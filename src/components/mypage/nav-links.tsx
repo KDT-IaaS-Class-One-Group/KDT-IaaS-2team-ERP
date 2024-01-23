adbf55aef3cd17e297310fae2ede4b1db8d997e4
leeyuan <dbdks32123@gmail.com>
2024-01-23 19:52:23 +0900

    마이페이지 사이드바

--- src/components/mypage/nav-links.tsx
+++ src/components/mypage/nav-links.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "@/styles/navlink.module.scss";
 
 const links = [
-  { name: '회원정보', href: '/mypage/myinfo' },
-  { name: '구독-캐쉬', href: '/mypage/subscription' },
+  { name: "회원정보", href: "/mypage/myinfo" },
+  { name: "구독 / 캐쉬", href: "/mypage/subscription" },
 ];
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log(pathname)
   return (
     <>
       {links.map((link) => (
         <Link
-        key={link.name}
-        href={link.href}
-        className={`${styles.linkbox} ${pathname === link.href ? styles.activeLink : ''}`}
+          key={link.name}
+          href={link.href}
+          className={`${styles.linkbox} ${
+            pathname === link.href ? styles.activeLink : ""
+          }`}
         >
           <p className={styles.linkname}>{link.name}</p>
         </Link>
--- src/components/mypage/sidenav.tsx
+++ src/components/mypage/sidenav.tsx
@@ -1,15 +1,5 @@
-import Link from 'next/link';
 import NavLinks from '@/components/mypage/nav-links'
-import { usePathname } from 'next/navigation';
 import styles from "@/styles/sidenav.module.scss"
-
-
-const links = [
-  { name: '정보관리', href: '/mypage/myinfo' },
-  { name: '캐쉬관리', href: '/mypage/mycash' },
-  { name: '구독관리', href: '/mypage/subscription' },
-];
-
 
 export default function SideNav() {
 
