cc7a69bc2b36dd6396f9cd95fe42d56f8b246182
leeyuan <dbdks32123@gmail.com>
2024-01-24 17:28:20 +0900

    결제완료 컴포넌트 자동 메인페이지 이동

--- src/components/payment/PaymentInfo.js
+++ src/components/payment/PaymentInfo.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from 'next/navigation';
 
 const PaymentInfo = ({ userName }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      router.push("/"); // Replace "/" with your main page URL
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div>
-      <h1>결제가 완료되었습니다.</h1>
+    <div
+      style={{
+        width:"100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>결제 완료</h1>
       <p>주문자 정보: {userName}</p>
     </div>
   );
