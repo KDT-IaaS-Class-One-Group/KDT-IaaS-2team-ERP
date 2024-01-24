07619d3b07b82dd8259f62c7bd2b059fa9d15cb0
leeyuan <dbdks32123@gmail.com>
2024-01-24 17:28:59 +0900

    마이페이지 구독 수정

--- src/app/mypage/subscription/page.tsx
+++ src/app/mypage/subscription/page.tsx
@@ -1,23 +1,23 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Index from '@/app/page';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import styles from "@/styles/mysub.module.scss"
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Index from "@/app/page";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import styles from "@/styles/mysub.module.scss";
 interface DataItem {
   auto_renew: number;
-  Product_Index:number,
-  Product_Index2:number,
-  Product_Index3:number,
-  address:string,
-  detailsaddress:string,
-  postcode:string,
-  productName1:string,
-  productName2:string,
-  productName3:string,
-  status:number,
-  user_name:string,
-  user_phone:string,
+  Product_Index: number;
+  Product_Index2: number;
+  Product_Index3: number;
+  address: string;
+  detailsaddress: string;
+  postcode: string;
+  productName1: string;
+  productName2: string;
+  productName3: string;
+  status: number;
+  user_name: string;
+  user_phone: string;
 }
 
 interface UserInfo {
@@ -40,18 +40,18 @@ export default function MyPagesub() {
   const [productData, setproductData] = useState<any>(null);
   const [subsStart, setSubsStart] = useState<string | null>(null);
   const [subsEnd, setSubsEnd] = useState<string | null>(null);
-  const [data, setData]  = useState<DataItem | null>(null);
+  const [data, setData] = useState<DataItem | null>(null);
 
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const [selectedCashAmount, setSelectedCashAmount] = useState<number>(0);
   const [isPaymentEnabled, setIsPaymentEnabled] = useState<boolean>(false);
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('token');
+    const storedToken = localStorage.getItem("token");
     setToken(storedToken);
 
     if (!storedToken) {
-      router.push('/login');
+      router.push("/login");
     } else {
       // 토큰에서 사용자 정보를 읽어와서 화면에 표시
       loadUserFromToken(storedToken);
@@ -63,19 +63,33 @@ export default function MyPagesub() {
       const decodedToken = jwt.decode(token) as JwtPayload;
 
       if (decodedToken) {
-        const { User_Index, userId, name, birthdate, phoneNumber, email, postcode , address, detailaddress, gender } = decodedToken;
-  
+        const {
+          User_Index,
+          userId,
+          name,
+          birthdate,
+          phoneNumber,
+          email,
+          postcode,
+          address,
+          detailaddress,
+          gender,
+        } = decodedToken;
+
         // 서버에서 사용자 정보를 가져오기 위한 요청 생성
-        const userResponse = await fetch(`/api/cashInfo?userIndex=${User_Index}`, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const userResponse = await fetch(
+          `/api/cashInfo?userIndex=${User_Index}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
         if (userResponse.ok) {
           const userData = await userResponse.json();
-  
+
           const userInfomation: UserInfo = {
             userId,
             name: userData.name,
@@ -88,21 +102,23 @@ export default function MyPagesub() {
             detailaddress: userData.detailaddress,
             gender: userData.gender,
           };
-        setUserInfo(userInfomation);
+          setUserInfo(userInfomation);
+        } else {
+          console.error(
+            "사용자 정보를 가져오는데 실패했습니다:",
+            userResponse.statusText
+          );
+          setUserInfo(null);
+        }
       } else {
-        console.error('사용자 정보를 가져오는데 실패했습니다:', userResponse.statusText);
+        console.error("유효하지 않은 토큰입니다.");
         setUserInfo(null);
       }
-    } else {
-      console.error('유효하지 않은 토큰입니다.');
+    } catch (error) {
+      console.error("토큰 해독 오류:", error);
       setUserInfo(null);
     }
-  } catch (error) {
-    console.error('토큰 해독 오류:', error);
-    setUserInfo(null);
-  }
-};
-
+  };
 
   const handleCashSelection = (amount: number) => {
     setSelectedCashAmount(amount);
@@ -111,17 +127,17 @@ export default function MyPagesub() {
 
   const handlePayment = async () => {
     if (!userInfo || selectedCashAmount === 0) {
-      console.error('사용자 정보 또는 선택된 캐쉬 금액이 유효하지 않습니다.');
+      console.error("사용자 정보 또는 선택된 캐쉬 금액이 유효하지 않습니다.");
       return;
     }
 
-    const currentToken = token || '';
+    const currentToken = token || "";
     try {
       // 서버로 결제 요청 보내기
-      const response = await fetch('/api/addCash', {
-        method: 'POST',
+      const response = await fetch("/api/addCash", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${currentToken}`,
         },
         body: JSON.stringify({
@@ -137,169 +153,188 @@ export default function MyPagesub() {
         setSelectedCashAmount(0);
         setIsPaymentEnabled(false);
       } else {
-        console.error('캐쉬 추가 실패:', response.statusText);
+        console.error("캐쉬 추가 실패:", response.statusText);
       }
     } catch (error) {
-      console.error('서버 에러:', error);
+      console.error("서버 에러:", error);
     }
   };
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('token');
+    const storedToken = localStorage.getItem("token");
     setToken(storedToken);
 
     if (!storedToken) {
-      router.push('/login');
+      router.push("/login");
     } else {
       fetchSubscriptionData(storedToken);
     }
   }, []);
 
   const formatDateString = (dateString: string) => {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return new Date(dateString).toLocaleDateString('ko-KR', options);
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    };
+    return new Date(dateString).toLocaleDateString("ko-KR", options);
   };
 
   const fetchSubscriptionData = async (token: string) => {
-  try {
-    const response = await fetch('/api/mysubscription', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
+    try {
+      const response = await fetch("/api/mysubscription", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
 
-    if (!response.ok) {
-    
-      console.error('Error fetching subscription data:', response.statusText);
-      return;
+      if (!response.ok) {
+        console.error("Error fetching subscription data:", response.statusText);
+        return;
+      }
+
+      const data = await response.json();
+      setSubscriptionData(data.subscriptionData);
+
+      const products = [
+        {
+          Product_Index: data.Product_Index,
+          productName: data.productName1,
+        },
+        {
+          Product_Index: data.Product_Index2,
+          productName: data.productName2,
+        },
+        {
+          Product_Index: data.Product_Index3,
+          productName: data.productName3,
+        },
+      ];
+
+      setData(data);
+      setproductData(products);
+      setSubsStart(formatDateString(data.Subs_Start));
+      setSubsEnd(formatDateString(data.Subs_End));
+    } catch (error) {
+      console.error("Error fetching subscription data:", error);
     }
+  };
 
-    const data = await response.json();
-    setSubscriptionData(data.subscriptionData);
-  
-    const products = [
-      {
-        Product_Index: data.Product_Index,
-        productName: data.productName1,
-      },
-      {
-        Product_Index: data.Product_Index2,
-        productName: data.productName2,
-      },
-      {
-        Product_Index: data.Product_Index3,
-        productName: data.productName3,
-      },
-    ];
+  const cancelSubscription = async () => {
+    try {
+      const response = await fetch("/api/cancelsubscription", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
 
-    setData(data)
-    setproductData(products);
-    setSubsStart(formatDateString(data.Subs_Start));
-    setSubsEnd(formatDateString(data.Subs_End));
-  } catch (error) {
-    console.error('Error fetching subscription data:', error);
-  }
-};
+      if (!response.ok) {
+        console.error("Error canceling subscription:", response.statusText);
+        return;
+      }
 
-const cancelSubscription = async () => {
-  try {
-    const response = await fetch('/api/cancelsubscription', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      console.error('Error canceling subscription:', response.statusText);
-      return;
+      const updatedData = await response.json();
+      alert("구독 서비스가 종료일에 해지됩니다. 다시 로그인해주세요.");
+      logout();
+    } catch (error) {
+      console.error("Error canceling subscription:", error);
     }
+  };
 
-    const updatedData = await response.json();
-    alert('구독 서비스가 종료일에 해지됩니다. 다시 로그인해주세요.');
-    logout();
-  } catch (error) {
-    console.error('Error canceling subscription:', error);
-  }
-};
-
-const logout = () => {
-  localStorage.removeItem('token'); 
-};
+  const logout = () => {
+    localStorage.removeItem("token");
+  };
 
   return (
     <div className={styles.root}>
       <div className={styles.subinfo}>
-            <h2 className={styles.title}>구독중인 서비스</h2>
-            {token && !subscriptionData && (
-            <p className={styles.title}>현재 구독 중인 서비스가 없습니다.</p>
-            )}
-            {subscriptionData && (
-              <div>
-                <p>{subscriptionData.name}</p>
-                <p>{subscriptionData.week}주 / {subscriptionData.price}원</p>
-              </div>
-            )}
+        <h2 className={styles.title}>구독 중인 서비스</h2>
+        {token && !subscriptionData && (
+          <p className={styles.title}>구독 중인 서비스 없음</p>
+        )}
+        {subscriptionData && (
+          <div>
+            <p>{subscriptionData.name}</p>
+            <p>
+              {subscriptionData.week}주 / {subscriptionData.price}원
+            </p>
+          </div>
+        )}
 
-            {subsStart && subsEnd && (
-              <div>
-                <p>구독 시작일: {subsStart}</p>
-                <p>구독 종료일: {subsEnd}</p>
-              </div>
-            )}
-      
-    
-            {data && data.auto_renew === 1 ? (
-              <div>
-                <p>{subsEnd && formatDateString(new Date(new Date(subsEnd).getTime() - 24 * 60 * 60 * 1000).toString())}에 결제 예정</p>
-                <button className={styles.btn} onClick={cancelSubscription}>구독 취소</button>
-              </div>
-            ) : (
-              <p>{subsEnd}에 서비스 해지 예정</p>
-            )}
+        {subsStart && subsEnd && (
+          <div>
+            <p>구독 시작일: {subsStart}</p>
+            <p>구독 종료일: {subsEnd}</p>
+          </div>
+        )}
+
+        {token && subscriptionData ? (
+  <div>
+    <p>
+      {subsEnd &&
+        formatDateString(
+          new Date(
+            new Date(subsEnd).getTime() - 24 * 60 * 60 * 1000
+          ).toString()
+        )}
+      에 {data && data.auto_renew === 1 ? '결제 예정' : '서비스 해지 예정'}
+    </p>
+    {data && data.auto_renew === 1 && (
+      <button className={styles.btn} onClick={cancelSubscription}>
+        구독 취소
+      </button>
+    )}
+  </div>
+) : null}
       </div>
       <div className={styles.productinfo}>
         {productData && productData.length > 0 && (
           <>
             <h2 className={styles.title}>구독 상품 목록</h2>
             <ul>
-              {productData.map((product: any, index: number) => (
-                // productName이 null이 아닌 경우에만 렌더링
-                product.productName && (
-                  <li key={index}>
-                    <p>{product.productName}</p>
-                  </li>
-                )
-              ))}
+              {productData.map(
+                (product: any, index: number) =>
+                  // productName이 null이 아닌 경우에만 렌더링
+                  product.productName && (
+                    <li key={index}>
+                      <p>{product.productName}</p>
+                    </li>
+                  )
+              )}
             </ul>
           </>
         )}
       </div>
       <div className={styles.cashinfo}>
-          {userInfo && (
-            <div>
-              <h3>{userInfo.name} 님</h3>
-              <p>나의 캐쉬 : {userInfo.cash}</p>
-            </div>
-          )}
+        {userInfo && (
+          <div>
+            <h3>{userInfo.name} 님</h3>
+            <p>나의 캐쉬 : {userInfo.cash}</p>
+          </div>
+        )}
 
         <div className={styles.addcash}>
-            <h3>캐쉬 충전</h3>
-            <select onChange={(e) => handleCashSelection(Number(e.target.value))}>
-              <option value="0">선택하세요</option>
-              <option value="5000">5,000원</option>
-              <option value="10000">10,000원</option>
-              <option value="30000">30,000원</option>
-              <option value="50000">50,000원</option>
-            </select>
-            <button className={styles.btn} onClick={handlePayment} disabled={!isPaymentEnabled}>
-              결제
-            </button>
-          </div>
+          <h3>캐쉬 충전</h3>
+          <select onChange={(e) => handleCashSelection(Number(e.target.value))}>
+            <option value="0">선택하세요</option>
+            <option value="5000">5,000원</option>
+            <option value="10000">10,000원</option>
+            <option value="30000">30,000원</option>
+            <option value="50000">50,000원</option>
+          </select>
+          <button
+            className={styles.btn}
+            onClick={handlePayment}
+            disabled={!isPaymentEnabled}
+          >
+            결제
+          </button>
         </div>
+      </div>
     </div>
   );
 }
