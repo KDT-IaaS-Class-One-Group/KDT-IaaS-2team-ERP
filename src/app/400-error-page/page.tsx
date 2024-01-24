51ef97b36163dfc5962094f1662301cc6d7880bf
leeyuan <dbdks32123@gmail.com>
2024-01-24 17:14:14 +0900

    잔액부족 페이지 2초 후 자동 캐쉬충전 페이지 이동

--- src/app/400-error-page/page.tsx
+++ src/app/400-error-page/page.tsx
@@ -1,8 +1,31 @@
-export default async function Page() {
-    return (
-      <div>
-        잔액이 부족합니다.
-      </div>
-    );
-  }
-  
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const PaymentInfo = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      router.push("/mypage/subscription"); // Replace "/" with your main page URL
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+  return (
+    <div
+      style={{
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop:"45vh"
+      }}
+    >
+      잔액이 부족합니다. 캐쉬 충전 후에 이용해주세요.
+    </div>
+  );
+};
+
+export default PaymentInfo;
